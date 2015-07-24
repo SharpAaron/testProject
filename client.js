ef23209d15f6269beff95fdc0a33b8e7ed46918f
Aaron Wasserman <aaronxiv@gmail.com>
2015-07-23 22:43:48 -0700

    [add] improved functionality

--- client.js
+++ client.js
@@ -1,22 +1,34 @@
 angular.module('angularModule', []).
-  controller('angularController', function($scope, $interval) {
+  controller('angularController', function($scope, $interval, $http) {
     $scope.arr = [];
+    $scope.isDisabled = false;
 
     $scope.randomGenerator = function(min, max) {
       return Math.floor(Math.random() * (max - min + 1) + min);
     };
 
     $scope.fillRandom = function() {
-      $scope.arr = [];
       for(var i = 0; i < 20; i++) {
-        $scope.arr.push($scope.randomGenerator(0, 10000));
+        $scope.arr.push({stuff: $scope.randomGenerator(0, 10000)});
       }
     };
 
     $scope.autoRefresh = function() {
+      $scope.isDisabled = true;
       $interval(function() {
         $scope.fillRandom();
-      }, 500);
+      }, 710);
+    };
+
+    $scope.getQuote = function() {
+      $http.get('http://api.acme.international/fortune').
+        success(function(data) {
+          $scope.autoRefresh();
+          $scope.arr.push(data.fortune);
+        }).
+        error(function() {
+          console.log("Failure");
+        });
     };
 
     $scope.fillRandom();
